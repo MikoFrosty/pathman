5fa1b35df55432a869db0e778ff8da2cae74bd57
brandon <srlguitarist@gmail.com>
2022-07-05 00:04:55 -0400

    Add smart node branch sorting by distance. Create global variable for field sizes, hat positions, and valid path. Add comments. Refactor player and hat start position code. Begin code for remebering valid path after solve. Format document. Add timeout for field rendering on 'new game' click to fix bug. Edit rendering rules to allow validated field to stay visible.

--- index.js
+++ index.js
@@ -6,17 +6,26 @@ async function runGame() {
   let density = 5;
   let speed = 0;
   let fieldSize = {
-    sizeArr: [[7, 10], [8, 12], [9, 14], [10, 16], [11, 18], [12,20]],
-    current: 5
-  }
+    sizeArr: [
+      [7, 10],
+      [8, 12],
+      [9, 14],
+      [10, 16],
+      [11, 18],
+      [12, 20],
+    ],
+    current: 5,
+  };
   let stop = false;
   let playerPosition = [0, 0];
+  let hatPosition = [0, 0];
+  let validPath = [];
 
   class Field {
     constructor(sizeArr) {
       this.world = Field.generateField(sizeArr[0], sizeArr[1]);
-      this.playerPosition = playerPosition; 
-      this.end = 'false';
+      this.playerPosition = playerPosition;
+      this.end = "false";
       this.height = sizeArr[0];
       this.width = sizeArr[1];
     }
@@ -25,13 +34,18 @@ async function runGame() {
       console.log(world.map((row) => row.join("")).join("\n"));
     }
 
+    // Create a field of the given size
     static generateField(height, width) {
-        document.querySelector("#validation-result").innerHTML = "";
+      // Text outputs
+      document.querySelector("#validation-result").innerHTML = "";
       console.log("Generating Field");
+      // Empty field
       let game = [];
+      // Random number generator
       function randomNum(min = 1, max = 10) {
         return Math.floor(Math.random() * (max - min + 1) + min);
       }
+      // Generate field user random number generator
       for (let i = 0; i < height; i++) {
         let row = [];
         for (let j = 0; j < width; j++) {
@@ -44,36 +58,40 @@ async function runGame() {
         }
         game.push(row);
       }
+      // Add hat
       let hatPosX = randomNum(0, width - 1);
-      let hatPosY = (height - 1);
+      let hatPosY = height - 1;
       game[hatPosY][hatPosX] = hat;
-      playerPosition = [
-        0,
-        randomNum(0, width - 1)
-      ];
-      game[playerPosition[0]][playerPosition[1]] = pathCharacter;
-
+      hatPosition[0] = hatPosY;
+      hatPosition[1] = hatPosX;
+      // Add starting location
+      let startPosX = randomNum(0, width - 1);
+      let startPosY = 0;
+      game[startPosY][startPosX] = pathCharacter;
+      playerPosition[0] = startPosY;
+      playerPosition[1] = startPosX;
+      // Return field
       return game;
     }
 
     async validateField() {
-      //Validation
+      // Text outputs
       console.log("Validating Field");
       document.querySelector("#validation-result").innerHTML = "VALIDATING...";
       const pathTable = document.querySelector("#path-table-body");
-
+      // Test world object
       const test = {
         path: [{ coords: this.playerPosition, nodes: [], check: 0 }],
         world: JSON.parse(JSON.stringify(this.world)), // world is an array of arrays // using json trick to deeply copy array
-        //position: this.playerPosition, // [y, x] // not used for new algorithm
         directions: ["up", "right", "left", "down"],
         valid: false,
       };
-      
+
       //main loop
       while (true) {
         // output path to pathTable
         pathTable.innerHTML = "";
+        // Iterate through current path list and add to pathTable along with corresponding nodes
         for (let i = 0; i < test.path.length; i++) {
           let path = test.path[i];
           let tr = document.createElement("tr");
@@ -87,20 +105,22 @@ async function runGame() {
           });
           tr.appendChild(branch);
         }
-        
+        // Halt if stop is true :: maybe add a pause and continue option ::
         if (stop) {
           break;
         }
-        // wait
+        // speed settings
         if (speed < 10) {
           await new Promise((resolve) => setTimeout(resolve, (9 - speed) * 20));
         }
+        // display game
         console.clear();
         game.print(test.world);
-        // loop forever - solution will be found and returned (true or false)
 
         // check winning and losing conditions
         if (test.valid) {
+          // add test path to validPath
+          validPath = test.path.map(path => path.coords);
           console.clear();
           console.log("Valid Field Found");
           break;
@@ -109,10 +129,10 @@ async function runGame() {
           console.log("Invalid Field");
           break; // break out of loop - valid remains false
         }
-        // last spot in path
+        // assign last spot in path to 'current'
         let current = test.path[test.path.length - 1];
 
-        // if current node hasn't been checked yet
+        // if current node hasn't been checked yet // each node should only be checked once
         if (!current.check) {
           // check all squares around current square for list of nodes & push to current nodes node list
           test.directions.forEach((direction) => {
@@ -185,6 +205,18 @@ async function runGame() {
             }
           }); // End of forEach
 
+          // sort current nodes by distance to hat
+          // this is basically an a* depth first search
+          current.nodes.sort((a, b) => {
+            let bDist =
+              Math.abs(a[0] - hatPosition[0]) +
+              Math.abs(a[1] - hatPosition[1]);
+            let aDist =
+              Math.abs(b[0] - hatPosition[0]) +
+              Math.abs(b[1] - hatPosition[1]);
+            return aDist - bDist;
+          });
+
           // make sure current position is a path character
           test.world[current.coords[0]][current.coords[1]] = pathCharacter;
           current.check = 1; // set current node to checked
@@ -205,6 +237,7 @@ async function runGame() {
           });
         }
       } // End of while loop
+      
       this.print(test.world);
       return test.valid;
     }
@@ -212,10 +245,10 @@ async function runGame() {
     _updatePosition() {
       let [y, x] = this.playerPosition;
       if (this.world[y][x] === hole) {
-        this.end = 'hole';
+        this.end = "hole";
         console.log("GAME OVER\nYou fell down a hole!");
       } else if (this.world[y][x] === hat) {
-        this.end = 'hat';
+        this.end = "hat";
       } else {
         this.world[y][x] = pathCharacter;
       }
@@ -292,18 +325,21 @@ async function runGame() {
       switch (id) {
         case "size-slider":
           fieldSize.current = value;
-          document.querySelector("#size-slider-label").innerHTML = "Size (HxW): " + JSON.stringify(fieldSize.sizeArr[value]);
+          document.querySelector("#size-slider-label").innerHTML =
+            "Size (HxW): " + JSON.stringify(fieldSize.sizeArr[value]);
           game = new Field(fieldSize.sizeArr[fieldSize.current]);
           render();
           break;
         case "density-slider":
-          document.querySelector("#density-slider-label").innerHTML = "Density: " + value;
+          document.querySelector("#density-slider-label").innerHTML =
+            "Density: " + value;
           density = value;
           game = new Field(fieldSize.sizeArr[fieldSize.current]);
           render();
           break;
         case "speed-slider":
-          document.querySelector("#speed-slider-label").innerHTML = value < 10 ? "Speed: " + value : "Speed: Instant";
+          document.querySelector("#speed-slider-label").innerHTML =
+            value < 10 ? "Speed: " + value : "Speed: Instant";
           speed = value;
           break;
       }
@@ -357,66 +393,88 @@ async function runGame() {
 
   async function optionButtons() {
     options.forEach((option) => {
-        option.addEventListener("click", (event) => {
-          (async () => {
-            switch (event.target.id) {
+      option.addEventListener("click", (event) => {
+        (async () => {
+          switch (event.target.id) {
             case "new":
+              stop = true;
               game = new Field(fieldSize.sizeArr[fieldSize.current]);
-              render();
+              // render is called after 200ms to allow time for the game to stop rendering test fields if busy validating
+              await new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve(render());
+                }, 200);
+              });
               break;
+            // SINGLE VALIDATION
             case "validate":
               event.target.disabled = true;
               stop = false;
               let tempVal = await game.validateField();
               if (tempVal) {
-                document.querySelector("#validation-result").innerHTML = "VALID FIELD FOUND";
-                } else {
-                    document.querySelector("#validation-result").innerHTML = "INVALID FIELD";
-                }
+                document.querySelector("#validation-result").innerHTML =
+                  "VALID FIELD FOUND";
+              } else {
+                document.querySelector("#validation-result").innerHTML =
+                  "INVALID FIELD";
+              }
               event.target.disabled = false;
-              render();
-              break;
-            case "auto":
-                event.target.disabled = true;
-                stop = false;
-                let tries = 1;
-                document.querySelector("#validation-result").innerHTML = "VALIDATING...";
-                while (!(await game.validateField())) {
-                    if (stop) {
-                      document.querySelector("#validation-result").innerHTML = "VALIDATION STOPPED | After " + tries + " tries";
-                      break;
-                    }
-                    if (tries >= 5000) {
-                      stop = true;
-                      document.querySelector("#validation-result").innerHTML = "VALIDATION STOPPED | Limit reached at 5000 tries";
-                      break;
-                    } else {
-                      tries++;
-                    } 
-                    document.querySelector("#validation-result").innerHTML = "INVALID FIELD | No possible path";
-                    game = new Field(fieldSize.sizeArr[fieldSize.current]);
-                    render();
-                }
+
               
-                event.target.disabled = false;
-                if (!stop) {
-                  document.querySelector("#validation-result").innerHTML = "VALID FIELD | Path found after " + tries + " tries";
+              //render(); // TEMP TEST // Allows user to see the field after validation
+              break;
+            // AUTO VALIDATION
+            case "auto":
+              event.target.disabled = true;
+              stop = false;
+              let tries = 1;
+              document.querySelector("#validation-result").innerHTML =
+                "VALIDATING...";
+              while (!(await game.validateField())) {
+                if (stop) {
+                  document.querySelector("#validation-result").innerHTML =
+                    "VALIDATION STOPPED | After " + tries + " tries";
                   break;
                 }
+                if (tries >= 5000) {
+                  stop = true;
+                  document.querySelector("#validation-result").innerHTML =
+                    "VALIDATION STOPPED | Limit reached at 5000 tries";
+                  break;
+                } else {
+                  tries++;
+                }
+                document.querySelector("#validation-result").innerHTML =
+                  "INVALID FIELD | No possible path";
+                game = new Field(fieldSize.sizeArr[fieldSize.current]);
+                render();
+              }
+
+              event.target.disabled = false;
+              if (!stop) {
+                document.querySelector("#validation-result").innerHTML =
+                  "VALID FIELD | Path found after " + tries + " tries";
+                break;
+              }
             case "stop":
               stop = true;
               break;
-            }
-            // blur button to remove focus
-            event.target.blur();
-            render();
+          }
+          // blur button to remove focus
+          event.target.blur();
+          //render(); // TEMP TEST // Allows user to see the field after validation
+          // Make final path red colored and show it on the field
+          validPath.forEach((coord) => {
+            game.field[coord[0]][coord[1]] = "<span class='red'>X</span>";
+          })
+          render();
+
         })();
-          });
       });
+    });
   }
 
   optionButtons();
-  
 
   function endCheck() {
     if (game.end === "hat") {
