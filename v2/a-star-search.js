fee6c147487cf7be10c9f7b105db4b1ccf5c0041
brandon <srlguitarist@gmail.com>
2022-07-06 19:55:04 -0400

    Copy code from depthFirst... as starting template

--- v2/a-star-search.js
+++ v2/a-star-search.js
@@ -1,167 +1,97 @@
-async validateField() {
-    // Text outputs
-    console.log("Validating Field");
-    document.querySelector("#validation-result").innerHTML = "VALIDATING...";
-    const pathTable = document.querySelector("#path-table-body");
-    // Test world object
-    const test = {
-      path: [{ coords: this.playerPosition, nodes: [], check: 0 }],
-      world: JSON.parse(JSON.stringify(this.world)), // world is an array of arrays // using json trick to deeply copy array
-      directions: ["up", "right", "left", "down"],
-      valid: false,
-    };
+import getNeighbors from "./get-neighbors.js";
 
-    //main loop
-    while (true) {
-      // output path to pathTable
-      pathTable.innerHTML = "";
-      // Iterate through current path list and add to pathTable along with corresponding nodes
-      for (let i = 0; i < test.path.length; i++) {
-        let path = test.path[i];
-        let tr = document.createElement("tr");
-        let td = document.createElement("td");
-        td.innerHTML = path.coords;
-        tr.appendChild(td);
-        pathTable.appendChild(tr);
-        let branch = document.createElement("td");
-        path.nodes.forEach((node) => {
-          branch.insertAdjacentHTML("beforeend", `&nbsp;${node}&nbsp;&nbsp;`);
-        });
-        tr.appendChild(branch);
-      }
-      // Halt if stop is true :: maybe add a pause and continue option ::
-      if (stop) {
-        break;
-      }
-      // speed settings
-      if (speed < 10) {
-        await new Promise((resolve) => setTimeout(resolve, (9 - speed) * 20));
-      }
-      // display game
-      console.clear();
-      game.print(test.world);
+export default async function depthFirstSearch(
+  fieldData,
+  nodeHTML,
+  render,
+  options
+) {
+  const { speed = 0, informed = false } = options;
+  const { field: originalField, start, goal } = fieldData;
+  const { floor, path, trail, neighbor, head, goal: goalHTML } = nodeHTML;
 
-      // check winning and losing conditions
-      if (test.valid) {
-        // add test path to validPath
-        validPath = test.path.map(path => path.coords);
-        console.clear();
-        console.log("Valid Field Found");
-        break;
-      } else if (test.path.length < 1) {
-        console.clear();
-        console.log("Invalid Field");
-        break; // break out of loop - valid remains false
-      }
-      // assign last spot in path to 'current'
-      let current = test.path[test.path.length - 1];
+  console.log("Searching for path...");
 
-      // if current node hasn't been checked yet // each node should only be checked once
-      if (!current.check) {
-        // check all squares around current square for list of nodes & push to current nodes node list
-        test.directions.forEach((direction) => {
-          // check if direction is valid
-          if (!this._boundryCheck(direction, current.coords)) {
-            switch (direction) {
-              case "up":
-                if (
-                  test.world[current.coords[0] - 1][current.coords[1]] === hat
-                ) {
-                  test.valid = true;
-                } else if (
-                  // check if square is a field character
-                  test.world[current.coords[0] - 1][current.coords[1]] ===
-                  fieldCharacter
-                ) {
-                  // add node to last path
-                  current.nodes.push([
-                    current.coords[0] - 1,
-                    current.coords[1],
-                  ]);
-                }
-                break;
-              case "right":
-                if (
-                  test.world[current.coords[0]][current.coords[1] + 1] === hat
-                ) {
-                  test.valid = true;
-                } else if (
-                  test.world[current.coords[0]][current.coords[1] + 1] ===
-                  fieldCharacter
-                ) {
-                  current.nodes.push([
-                    current.coords[0],
-                    current.coords[1] + 1,
-                  ]);
-                }
-                break;
-              case "down":
-                if (
-                  test.world[current.coords[0] + 1][current.coords[1]] === hat
-                ) {
-                  test.valid = true;
-                } else if (
-                  test.world[current.coords[0] + 1][current.coords[1]] ===
-                  fieldCharacter
-                ) {
-                  current.nodes.push([
-                    current.coords[0] + 1,
-                    current.coords[1],
-                  ]);
-                }
-                break;
-              case "left":
-                if (
-                  test.world[current.coords[0]][current.coords[1] - 1] === hat
-                ) {
-                  test.valid = true;
-                } else if (
-                  test.world[current.coords[0]][current.coords[1] - 1] ===
-                  fieldCharacter
-                ) {
-                  current.nodes.push([
-                    current.coords[0],
-                    current.coords[1] - 1,
-                  ]);
-                }
-                break;
-            }
-          }
-        }); // End of forEach
+  // Test world object
+  const field = JSON.parse(JSON.stringify(originalField)); // json trick to deeply copy array (In case original field is reused later)
+  const openList = [{ coords: start, nodes: [], check: 0 }];
+  const closedList = [];
 
-        // sort current nodes by distance to hat
-        // this is basically an a* depth first search
-        current.nodes.sort((a, b) => {
-          let bDist =
-            Math.abs(a[0] - hatPosition[0]) +
-            Math.abs(a[1] - hatPosition[1]);
-          let aDist =
-            Math.abs(b[0] - hatPosition[0]) +
-            Math.abs(b[1] - hatPosition[1]);
-          return aDist - bDist;
-        });
+  //main loop
+  while (true) {
+    // assign last spot in path to 'current' (if path exists), and destructure
+    let current = openList[openList.length - 1] ?? null;
+    let [y, x] = current?.coords ?? [null, null];
 
-        // make sure current position is a path character
-        test.world[current.coords[0]][current.coords[1]] = pathCharacter;
-        current.check = 1; // set current node to checked
-      } // End of if
+    // speed settings
+    if (speed < 10) {
+      await new Promise((resolve) => setTimeout(resolve, (9 - speed) * 20));
+    }
 
-      // check if current path has nodes
-      if (!current.nodes.length) {
-        // if no nodes, pop off last path
-        test.path.pop();
-        console.log("Backtracking");
+    // Orange trail effect to show current best path found
+    let currentTrail = openList.map((node) => node.coords);
+    currentTrail.forEach((coords) => {
+      if (coords === start) {
+      } else if (coords === currentTrail[currentTrail.length - 1]) {
+        field[y][x] = head;
       } else {
-        // if there are nodes
-        // remove last node from current path
-        test.path.push({
-          coords: current.nodes.pop(),
-          nodes: [],
-          check: 0,
-        });
+        field[coords[0]][coords[1]] = trail;
       }
-    } // End of while loop
-    
-    this.print(test.world);
-    return test.valid;
-  }
+    });
+
+    // display game
+    render(field);
+
+    // check winning and losing conditions
+    if ([y, x].toString() === goal.toString()) {
+      field[y][x] = goalHTML;
+      console.log("Path found!");
+      break;
+    } else if (openList.length < 1) {
+      console.log("No path found!");
+      break;
+    }
+
+    // make sure current position updates and shows path
+    if (
+      [y, x].toString() !== start.toString() &&
+      [y, x].toString() !== goal.toString()
+    ) {
+      field[y][x] = path;
+    }
+
+    // if current node hasn't been checked yet // each node should only be checked once
+    if (!current.check) {
+      console.log("Finding nodes");
+      current.nodes = getNeighbors(
+        { field, floor, goal },
+        current.coords,
+        informed
+      );
+      current.check = 1;
+    }
+
+    // change current.nodes to neighbor in field
+    current.nodes.forEach((node) => {
+      field[node[0]][node[1]] = neighbor;
+    });
+
+    // check if current path has nodes :: Make into a while loop
+    if (!current.nodes.length) {
+      // if no nodes, pop off last path
+      openList.pop();
+      console.log("Backtracking...");
+    } else {
+      // if there are nodes
+      // remove last node from current path
+      openList.push({
+        coords: current.nodes.pop(),
+        nodes: [],
+        check: 0,
+      });
+    }
+  } // End of while loop
+
+  render(field);
+  return null;
+}
