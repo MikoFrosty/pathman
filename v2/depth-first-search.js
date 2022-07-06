93c2ccf7560a62d2f41389b65f2a4c96aea5ae30
brandon <srlguitarist@gmail.com>
2022-07-06 19:52:54 -0400

    Remove redundant path color changes. Refactor parameters

--- v2/depth-first-search.js
+++ v2/depth-first-search.js
@@ -2,72 +2,52 @@ import getNeighbors from "./get-neighbors.js";
 
 export default async function depthFirstSearch(
   fieldData,
-  speed = 0,
+  nodeHTML,
   render,
-  informed = false,
-  nodeHTML
+  options
 ) {
+  const { speed = 0, informed = false } = options;
   const { field: originalField, start, goal } = fieldData;
-  const { floor, path, trail, neighbor, head } = nodeHTML;
+  const { floor, path, trail, neighbor, head, goal: goalHTML } = nodeHTML;
 
   console.log("Searching for path...");
 
   // Test world object
   const field = JSON.parse(JSON.stringify(originalField)); // json trick to deeply copy array (In case original field is reused later)
   const pathNodes = [{ coords: start, nodes: [], check: 0 }];
-  let valid = false; // not currently used
-  //let counter = 0; // for debugging
 
   //main loop
   while (true) {
     // assign last spot in path to 'current' (if path exists), and destructure
     let current = pathNodes[pathNodes.length - 1] ?? null;
     let [y, x] = current?.coords ?? [null, null];
-    let tempPath = []; // for storing true path data while head is moving
-
-    // swap current with temporary element to see where the head is
-    if (current) {
-      tempPath = field[y][x];
-      field[y][x] = head;
-    }
 
     // speed settings
     if (speed < 10) {
       await new Promise((resolve) => setTimeout(resolve, (9 - speed) * 20));
     }
 
-    /*
-    counter++; // for debugging
-    if (counter === 10) {
-      break;
-    }
-    */
+    // Orange trail effect to show current best path found
+    let currentTrail = pathNodes.map((node) => node.coords);
+    currentTrail.forEach((coords) => {
+      if (coords === start) {
+      } else if (coords === currentTrail[currentTrail.length - 1]) {
+        field[y][x] = head;
+      } else {
+        field[coords[0]][coords[1]] = trail;
+      }
+    });
 
     // display game
     render(field);
-    if (current) {
-      field[y][x] = tempPath;
-    }
-
-    // Orange trail effect to show current best path found
-    let currentTrail = pathNodes.map((node) => node.coords);
-    for (let coords of currentTrail) {
-      if (coords === start) {
-        continue;
-      }
-      field[coords[0]][coords[1]] = trail;
-    }
 
     // check winning and losing conditions
     if ([y, x].toString() === goal.toString()) {
-      field[y][x] = `<div id="goal" data-type="goal"></div>`;
+      field[y][x] = goalHTML;
       console.log("Path found!");
-
-      valid = true;
       break;
     } else if (pathNodes.length < 1) {
       console.log("No path found!");
-      valid = false;
       break;
     }
 
@@ -82,7 +62,11 @@ export default async function depthFirstSearch(
     // if current node hasn't been checked yet // each node should only be checked once
     if (!current.check) {
       console.log("Finding nodes");
-      current.nodes = getNeighbors({ field, floor, goal }, current.coords, informed);
+      current.nodes = getNeighbors(
+        { field, floor, goal },
+        current.coords,
+        informed
+      );
       current.check = 1;
     }
 
