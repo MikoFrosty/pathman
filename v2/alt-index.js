c5d23d2283d7ea83228d93da6fd9ccc545ef91a3
brandon <srlguitarist@gmail.com>
2022-07-09 22:54:18 -0400

    Offload funcs to helpers.js and edit func params

--- v2/alt-index.js
+++ v2/alt-index.js
@@ -2,32 +2,28 @@ import depthFirstSearch from "./depth-first-search.js";
 import generateField from "./generate-field.js";
 import aStarSearch from "./a-star-search.js";
 
-const nodeHTML = {
-  floor: `<div class="floor" data-type="floor"></div>`,
-  wall: `<div class="wall" data-type="wall"></div>`,
-  path: `<div class="path" data-type="path"></div>`,
-  trail: `<div class="trail" data-type="trail"></div>`,
-  neighbor: `<div class="neighbor" data-type="neighbor"></div>`,
-  head: `<div class="head" data-type="head"></div>`,
-  start: `<div id="start" data-type="start"></div>`,
-  goal: `<div id="goal" data-type="goal"></div>`
-}
-
 const searchOptions = {
   speed: 5,
-  informed: true
-}
+  informed: true,
+};
 
-// Depth First Search params: field object (field, start, goal), nodeHTML for each type of node, DOM display function, 
+const generateOptions = {
+  density: 50,
+  size: 10,
+};
+
+// start a new field and run aStarSearch on it in a loop until a path is found
+(async () => {
+  let field = generateField(generateOptions);
+  while (await aStarSearch(field, searchOptions) === false) {
+    field = generateField(generateOptions);
+  }
+})();
+
+// Generate new field
+//const field = generateField(generateOptions);
+
+// Depth First Search params: field object (field, start, goal), DOM display function,
 // and options (speed & informed search (true/false))
-//depthFirstSearch(generateField(nodeHTML, 30, 10), nodeHTML, renderField, searchOptions);
-aStarSearch(generateField(nodeHTML, 30, 10), nodeHTML, renderField, searchOptions);
-
-
-// print field to DOM
-function renderField(field) {
-  const fieldDisplay = document.querySelector("#field");
-  const fieldHTML = field.map((row) => row.join("")).join("<br />");
-  fieldDisplay.innerHTML = fieldHTML
-  //console.log(fieldHTML); // Used for console testing
-}
+//depthFirstSearch(generateField(nodeHTML, 30, 10), nodeHTML, searchOptions);
+//aStarSearch(field, searchOptions);
