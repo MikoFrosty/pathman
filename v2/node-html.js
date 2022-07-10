3d23e208368ad9a20209881aad1ac72d8f404730
brandon <srlguitarist@gmail.com>
2022-07-09 22:57:38 -0400

    Initial commit

---
+++ v2/node-html.js
@@ -0,0 +1,12 @@
+const nodeHTML = {
+  floor: `<div class="floor" data-type="floor"></div>`,
+  wall: `<div class="wall" data-type="wall"></div>`,
+  path: `<div class="path" data-type="path"></div>`,
+  trail: `<div class="trail" data-type="trail"></div>`,
+  neighbor: `<div class="neighbor" data-type="neighbor"></div>`,
+  head: `<div class="head" data-type="head"></div>`,
+  start: `<div id="start" data-type="start"></div>`,
+  goal: `<div id="goal" data-type="goal"></div>`,
+};
+
+export default nodeHTML;
