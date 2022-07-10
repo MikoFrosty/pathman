51b64cf782af65e15211a6e56d35011c4c85b6a3
brandon <srlguitarist@gmail.com>
2022-07-10 12:32:16 -0400

    Edit data-types for easier understanding

--- v2/node-html.js
+++ v2/node-html.js
@@ -1,8 +1,8 @@
 const nodeHTML = {
   floor: `<div class="floor" data-type="floor"></div>`,
   wall: `<div class="wall" data-type="wall"></div>`,
-  path: `<div class="path" data-type="path"></div>`,
-  trail: `<div class="trail" data-type="trail"></div>`,
+  path: `<div class="path" data-type="explored"></div>`,
+  trail: `<div class="trail" data-type="path"></div>`,
   neighbor: `<div class="neighbor" data-type="neighbor"></div>`,
   head: `<div class="head" data-type="head"></div>`,
   start: `<div id="start" data-type="start"></div>`,
