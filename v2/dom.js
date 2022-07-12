b645885a33630afd598476782a51fd5b254c6f5c
brandon <srlguitarist@gmail.com>
2022-07-11 22:51:55 -0400

    Init commit, (more clarity with helpers.js)

---
+++ v2/dom.js
@@ -0,0 +1,65 @@
+const dom = {
+    out1: {
+        status: document.querySelector("#status1"),
+        display: document.querySelector("#display1"),
+        search: document.querySelector("#search1"),
+    },
+    out2: {
+        status: document.querySelector("#status2"),
+        display: document.querySelector("#display2"),
+        search: document.querySelector("#search2")
+    },
+    out3: {
+        status: document.querySelector("#status3"),
+        display: document.querySelector("#display3"),
+        search: document.querySelector("#search3")
+    },
+    out4: {
+        status: document.querySelector("#status4"),
+        display: document.querySelector("#display4"),
+        search: document.querySelector("#search4")
+    }
+};
+
+export default dom;
+
+export function nodeHoverStats(field, display) {
+  const nodes = display.querySelectorAll("div");
+  nodes.forEach((node) => {
+    // find position in nodelist
+    const index = Array.prototype.indexOf.call(nodes, node);
+    // find position in field
+    const [nodeY, nodeX] = [
+      Math.floor(index / field[0].length),
+      index % field[0].length,
+    ];
+
+    // display array position when mouse hover node
+    const nodeData = document.querySelector(".node-data");
+
+    node.addEventListener("mouseover", showNodeData);
+    node.addEventListener("mouseleave", hideNodeData);
+
+    function showNodeData(e) {
+      nodeData.style.display = "block";
+      //make nodeData position next to mouse position
+      nodeData.style.top = e.clientY - 60 + "px";
+      nodeData.style.left = e.clientX - 60 + "px";
+      // nodeData position by finger position on mobile
+      nodeData.innerHTML = `${e.currentTarget.dataset.type}: y-${nodeY}, x-${nodeX}`;
+    }
+
+    function hideNodeData() {
+      nodeData.style.display = "none";
+      nodeData.innerHTML = "";
+    }
+  });
+}
+
+// print field to DOM
+export function renderField(field, oldField, display) {
+  // oldField will be used for possible smart updating in the future
+  // Possibly use this area for removing event listeners (if exist) and adding event listensers for the node squares
+  const fieldHTML = field.map((row) => row.join("")).join("<br />");
+  display.innerHTML = fieldHTML;
+}
