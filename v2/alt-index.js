2b0d5d7164bbee1e22f8a84a882f024c960a0259
brandon <srlguitarist@gmail.com>
2022-07-11 22:49:56 -0400

    Remove func (move), add output to searchSettings

--- v2/alt-index.js
+++ v2/alt-index.js
@@ -1,68 +1,48 @@
 import depthFirstSearch from "./depth-first-search.js";
 import generateField from "./generate-field.js";
 import aStarSearch from "./a-star-search.js";
+import dom, { nodeHoverStats } from "./dom.js";
 
 const searchOptions = {
-  speed: 5,
+  speed: 0,
   informed: true,
+  output: dom.out1  
 };
-
-const generateOptions = {
+const genOptions = {
   density: 30,
-  size: 10,
+  size: 3,
 };
 
-// start a new field and run aStarSearch on it in a loop until a path is found
-(async () => {
+let field = generateField(genOptions);
+
+findPath(field, aStarSearch, { searchOptions, genOptions, once: false });
+findPath(field, depthFirstSearch, {
+  searchOptions: { ...searchOptions, output: dom.out2 },
+  genOptions,
+  once: false,
+});
+
+// start a new field and run search on it in a loop until a path is found, or once if once = true
+async function findPath(field, search, options) {
+  const { searchOptions, genOptions, once } = options;
   let pathFound = false;
   while (!pathFound) {
-    let field = generateField(generateOptions);
-    await aStarSearch(field, searchOptions).then((data) => {
-      if (data) {
+    await search(field, searchOptions).then((data) => {
+      if (data || once) {
         pathFound = true;
-        nodeHoverStats(field.field);
+        nodeHoverStats(field.field, searchOptions.output.display);
+      } else {
+        searchOptions.output.status.textContent = "Generating Field";
+        field = generateField(genOptions);
       }
     });
   }
-})();
+}
 
 // Generate new field
 //const field = generateField(generateOptions);
 
 // Depth First Search params: field object (field, start, goal), DOM display function,
 // and options (speed & informed search (true/false))
-//depthFirstSearch(generateField(nodeHTML, 30, 10), nodeHTML, searchOptions);
+//depthFirstSearch(field, searchOptions);
 //aStarSearch(field, searchOptions);
-
-function nodeHoverStats(field) {
-  const nodes = document.querySelectorAll("#field>div");
-  nodes.forEach((node) => {
-    // find position in nodelist
-    const index = Array.prototype.indexOf.call(nodes, node);
-    // find position in field
-    const [nodeY, nodeX] = [
-      Math.floor(index / field[0].length),
-      index % field[0].length,
-    ];
-    
-    // display array position when mouse hover node
-    const nodeData = document.querySelector("#node-data");
-    
-    node.addEventListener("mouseover", showNodeData);
-    node.addEventListener("mouseleave", hideNodeData);
-    
-    function showNodeData(e) {
-      nodeData.style.display = "block";
-      //make nodeData position next to mouse position
-      nodeData.style.top = e.clientY - 60 + "px";
-      nodeData.style.left = e.clientX - 60 + "px";
-      // nodeData position by finger position on mobile
-      nodeData.innerHTML = `${e.currentTarget.dataset.type}: y-${nodeY}, x-${nodeX}`;
-    }
-
-    function hideNodeData() {
-      nodeData.style.display = "none";
-      nodeData.innerHTML = "";
-    }
-  });
-}
