a8be732ad06a4cfb7da84c09287738007dff9d67
brandon <srlguitarist@gmail.com>
2022-07-12 22:57:57 -0400

    Add pre-field validation before algos display. Remove commented out code

--- v2/alt-index.js
+++ v2/alt-index.js
@@ -13,54 +13,74 @@ const genOptions = {
   size: 1,
 };
 
-let field = generateField(genOptions);
-
-// Breadth First
-findPath(field, aStarSearch, {
-  searchOptions: { ...searchOptions, informed: false, output: dom.out1 },
+// hide display 1 while seaching for valid field
+for (let element in dom.out1) {
+  dom.out1[element].style.display = "none";
+}
+findPath(generateField(genOptions), aStarSearch, {
+  searchOptions: {
+    ...searchOptions,
+    speed: 10,
+    informed: true,
+    output: dom.out1,
+  },
   genOptions,
-  once: true,
-});
-// Depth First
-findPath(field, depthFirstSearch, {
-  searchOptions: { ...searchOptions, informed: false, output: dom.out2 },
-  genOptions,
-  once: true,
-});
-// A*
-findPath(field, aStarSearch, {
-  searchOptions: { ...searchOptions, informed: true, output: dom.out3 },
-  genOptions,
-  once: true,
-});
-// Best Depth First
-findPath(field, depthFirstSearch, {
-  searchOptions: { ...searchOptions, informed: true, output: dom.out4 },
-  genOptions,
-  once: true,
+  once: false,
+}).then((field) => {
+  // show display 1 (after short delay to let algorithms spin up)
+  setTimeout(() => {
+  for (let element in dom.out1) {
+    dom.out1[element].style.display = "block";
+  }}, 200);
+  // Dijkstra
+  findPath(field, aStarSearch, {
+    searchOptions: { ...searchOptions, informed: false, output: dom.out1 },
+    genOptions,
+    once: true,
+  });
+  // Depth First
+  findPath(field, depthFirstSearch, {
+    searchOptions: { ...searchOptions, informed: false, output: dom.out2 },
+    genOptions,
+    once: true,
+  });
+  // A*
+  findPath(field, aStarSearch, {
+    searchOptions: { ...searchOptions, informed: true, output: dom.out3 },
+    genOptions,
+    once: true,
+  });
+  // Best Depth First
+  findPath(field, depthFirstSearch, {
+    searchOptions: { ...searchOptions, informed: true, output: dom.out4 },
+    genOptions,
+    once: true,
+  });
 });
 
 // start a new field and run search on it in a loop until a path is found, or once if once = true
 async function findPath(field, search, options) {
   const { searchOptions, genOptions, once } = options;
   let pathFound = false;
+  let loops = 0;
   while (!pathFound) {
+    loops++;
+    if (loops > 100) {
+      console.log("100 loops exceeded, stopping search");
+      break;
+    }
     await search(field, searchOptions).then((data) => {
-      if (data || once) {
-        pathFound = true;
+      if (data) {
         nodeHoverStats(field.field, searchOptions.output.display);
-      } else {
+        pathFound = true;
+      } else if (!data && !once) {
         searchOptions.output.status.textContent = "Generating Field";
         field = generateField(genOptions);
       }
     });
+    if (once) {
+      break;
+    }
   }
+  return field;
 }
-
-// Generate new field
-//const field = generateField(generateOptions);
-
-// Depth First Search params: field object (field, start, goal), DOM display function,
-// and options (speed & informed search (true/false))
-//depthFirstSearch(field, searchOptions);
-//aStarSearch(field, searchOptions);
