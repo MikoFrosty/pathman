46c910666be74038280799e933a1b50549278f73
brandon <srlguitarist@gmail.com>
2022-07-13 20:13:25 -0400

    Remove unused code

--- v2/helpers.js
+++ v2/helpers.js
@@ -32,31 +32,3 @@ export function createNode(coords, parent, goal) {
 export function randomNum(min = 1, max = 100) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-// Not sure what to do with this yet
-/*
-  const nodes = document.querySelectorAll("#field>div");
-  nodes.forEach((node) => {
-    // find position in nodelist
-    const index = Array.prototype.indexOf.call(nodes, node);
-    // find position in field
-    const [nodeY, nodeX] = [
-      Math.floor(index / field[0].length),
-      index % field[0].length,
-    ];
-    // display array position when mouse hover node
-    const nodeData = document.querySelector("#node-data");
-    node.addEventListener("mouseover", (e) => {
-      nodeData.style.display = "block";
-      //make nodeData position next to mouse position
-      nodeData.style.top = e.clientY - 60 + "px";
-      nodeData.style.left = e.clientX - 60 + "px";
-      // nodeData position by finger position on mobile
-      nodeData.innerHTML = `${e.currentTarget.dataset.type}: y-${nodeY}, x-${nodeX}`;
-    });
-    node.addEventListener("mouseleave", (e) => {
-      nodeData.style.display = "none";
-      nodeData.innerHTML = "";
-    });
-  });
-  */
