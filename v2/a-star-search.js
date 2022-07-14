62f019a96f8944b7af530236d57903907275a7c0
brandon <srlguitarist@gmail.com>
2022-07-14 00:05:56 -0400

    Move stopOnce to dom.js & add comments. Remove reduntant trail re-pathing

--- v2/a-star-search.js
+++ v2/a-star-search.js
@@ -1,9 +1,11 @@
 import dealWithNeighbors from "./get-neighbors-advanced.js";
 import nodeHTML from "./node-html.js";
 import { createNode } from "./helpers.js";
-import dom, { renderField as render } from "./dom.js";
+import dom, { renderField as render, stopOnce } from "./dom.js";
 
+// MAIN FUNCTION
 export default async function aStarSearch(fieldData, options) {
+  // Destructuring parameters & nodeHTML
   const { field: originalField, start, goal } = fieldData;
   let {
     speed,
@@ -12,10 +14,11 @@ export default async function aStarSearch(fieldData, options) {
   } = options;
   const { floor, path, trail, neighbor, head, goal: goalHTML } = nodeHTML;
 
+  // Display text
   search.textContent = `${informed ? "A*" : "Dijkstra"}`;
   status.textContent = "Searching for path...";
 
-  // Test world object
+  // Creating variables & objects. Push start node to openList
   const field = originalField.map((array) => [...array]);
   let openList = [];
   let closedList = [];
@@ -23,33 +26,14 @@ export default async function aStarSearch(fieldData, options) {
   openList.push(startNode);
   let pathFound = false;
   let steps = 0;
-  let stop = false;
-  dom.start.addEventListener(
-    "click",
-    () => {
-      stop = true;
-    },
-    { once: true }
-  );
-  dom.stop.addEventListener(
-    "click",
-    () => {
-      stop = true;
-    },
-    { once: true }
-  );
-  dom.new.addEventListener(
-    "click",
-    () => {
-      stop = true;
-    },
-    { once: true }
-  );
+  let stop = [false];
+  // Stop on button press
+  stopOnce([dom.start, dom.stop, dom.new], stop);
 
-  //main loop
+  // MAIN LOOP
   while (openList.length) {
     // Break if stop is clicked
-    if (stop) {
+    if (stop[0]) {
       break;
     }
     steps++;
@@ -64,33 +48,25 @@ export default async function aStarSearch(fieldData, options) {
       await new Promise((resolve) => setTimeout(resolve, (9 - speed) * 20));
     }
 
-    // start at goal and trace back to start
-    // trailmarker is last item in closedList
+    // Setting up current trail
     let trailMarker = closedList[closedList.length - 1];
     let currentPath = [];
-
-    // Show head of current path
+    // Show head of current trail
     if (
       current.coords !== start &&
       current.coords === closedList[closedList.length - 1].coords
     ) {
       field[y][x] = head;
     }
-
-    // push all parents of trailMarker to currentPath
+    // Push all parents of trailMarker to currentPath. Convert to trail squares (orange)
     while (trailMarker.parent && trailMarker.parent !== startNode) {
       trailMarker = trailMarker.parent;
       currentPath.push(trailMarker.coords);
       field[trailMarker.coords[0]][trailMarker.coords[1]] = trail;
     }
 
-    // display game
+    // Display game
     render(field, originalField, display);
-
-    // Change to path (explored square)
-    currentPath.forEach((coords) => {
-      field[coords[0]][coords[1]] = path;
-    });
 
     // check winning and losing conditions
     if (y === goal[0] && x === goal[1]) {
@@ -98,19 +74,20 @@ export default async function aStarSearch(fieldData, options) {
       field[y][x] = goalHTML;
       status.textContent = `Path found! Loops: ${steps} - Path length: ${currentPath.length}`;
       pathFound = true;
-      // Show final trail
-      currentPath.forEach((coords) => {
-        field[coords[0]][coords[1]] = trail;
-      });
       // Break out of while loop
       break;
     }
 
-    // change head back to path
+    // Change back to path (explored green square)
+    currentPath.forEach((coords) => {
+      field[coords[0]][coords[1]] = path;
+    });
+    // Change head back to path
     if (current !== startNode) {
       field[y][x] = path;
     }
 
+    // Display status message
     status.textContent = "Finding neighbors...";
     // Find all neighbors of current node, and push to openList. Sort openList. Add neighbors to field.
     dealWithNeighbors(
@@ -124,7 +101,9 @@ export default async function aStarSearch(fieldData, options) {
   if (!pathFound) {
     status.textContent = "No path found!";
   }
+  // Final render
   render(field, originalField, display);
 
+  // (true/false)
   return pathFound;
 }
